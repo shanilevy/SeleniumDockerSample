9e9907408906e8c69d357220032705b96be00e77
Shani Levy <shaniiiiii@gmail.com>
2021-09-16 19:17:25 +1000

    Update DockerBasic.java

--- src/test/java/sample/DockerBasic.java
+++ src/test/java/sample/DockerBasic.java
@@ -17,7 +17,7 @@ public class DockerBasic {
     @Test(enabled = true)
     public void googleTest1() throws Throwable{
         //WebDriverManager.chromedriver().setup();
-        System.setProperty("webdriver.chrome.driver","/app/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
@@ -46,35 +46,4 @@ public class DockerBasic {
         driver.quit();
     }
 
-    @Test(enabled=true)
-    public void googleTest2() throws Throwable{
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions firefoxDockerOptions = new FirefoxOptions();
-        System.setProperty("webdriver.gecko.driver","/app/bin/geckodriver");
-        firefoxDockerOptions.addArguments("--headless");
-        firefoxDockerOptions.addArguments("--no-sandbox");
-        System.setProperty("webdriver.gecko.args", "--disable-logging");
-        System.setProperty("webdriver.gecko.silentOutput", "true");
-        firefoxDockerOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        firefoxDockerOptions.addArguments("disable-infobars"); // disabling infobars
-        firefoxDockerOptions.addArguments("--disable-extensions"); // disabling extensions
-        firefoxDockerOptions.addArguments("--disable-gpu"); // applicable to windows os only
-        firefoxDockerOptions.addArguments("--width=1920"); // Bypass OS security model
-        firefoxDockerOptions.addArguments("--height=1080");
-        FirefoxDriver driver = new FirefoxDriver(firefoxDockerOptions);
-        //WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.google.com");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-        driver.findElement(By.name("q")).sendKeys("Stop Watch");
-        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("(//span[contains(@class,'act-tim-resume-lbl')])[1]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("(//span[contains(@class,'act-tim-pause-lbl')])[1]")).click();
-        String text = driver.findElement(By.xpath("(//div[@class='act-tim-txt-cnt'])[1]")).getText();
-        System.out.println(text);
-        Thread.sleep(2000);
-        driver.quit();
-    }
 }
